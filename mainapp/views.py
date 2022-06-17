f146ad3af24e6e2a6fc2780c714b9847ebfc3a4f
Onlyjoysprod <kujaku@yandex.ru>
2022-06-17 19:28:21 +0300

    added templates and statics

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        # 'title': 'мой магазин',
+        # 'products': products_list,
+    }
+    return render(request, 'mainapp/index.html', context)
